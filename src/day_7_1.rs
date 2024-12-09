90ab34caad5a4306a045b620e2f8258674342073
Jeffrey René Ouimet <profaurore@gmail.com>
2024-12-09 13:18:55 -0500

    Day 7: Bridge Repair

---
+++ src/day_7_1.rs
@@ -0,0 +1,67 @@
+use std::fs;
+
+const OPS: [fn(u64, u64) -> u64; 2] =
+    [u64::saturating_mul, u64::saturating_add];
+
+/// https://adventofcode.com/2024/day/7#part1
+pub fn day_7_1() {
+    let data = fs::read_to_string("data/day_7.txt").expect("missing file");
+    let lines = data.split('\n');
+
+    let valid_test_values = lines
+        .filter_map(|line| {
+            line.split_once(": ")
+                .and_then(|(test_value_str, operands_str)| {
+                    test_value_str.parse::<u64>().ok().map(|test_value| {
+                        (
+                            test_value,
+                            operands_str
+                                .split(' ')
+                                .filter_map(|operand| {
+                                    operand.parse::<u64>().ok()
+                                })
+                                .collect::<Vec<_>>(),
+                        )
+                    })
+                })
+        })
+        .fold(0, |valid, (test_value, operands)| {
+            if operands.is_empty() {
+                return valid;
+            }
+
+            let num_ops = OPS.len();
+            let num_operands = operands.len();
+            let mut operators: Vec<usize> = vec![num_ops - 1; num_operands - 1];
+
+            loop {
+                let total = operators
+                    .iter()
+                    .zip(operands.iter().skip(1))
+                    .fold(operands[0], |total, (&op_index, &value)| {
+                        OPS[op_index](total, value)
+                    });
+
+                if total == test_value {
+                    return valid + test_value;
+                }
+
+                if operators.iter().sum::<usize>() == 0 {
+                    break;
+                }
+
+                for operator in operators.iter_mut().rev() {
+                    if *operator == 0 {
+                        *operator = num_ops - 1;
+                    } else {
+                        *operator -= 1;
+                        break;
+                    }
+                }
+            }
+
+            valid
+        });
+
+    println!("{}", valid_test_values);
+}
--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ mod day_5_1;
 mod day_5_2;
 mod day_6_1;
 mod day_6_2;
+mod day_7_1;
+mod day_7_2;
 mod day_not_implemented;
 
 use std::env;
@@ -22,6 +24,7 @@ use crate::{
     day_1_1::day_1_1, day_1_2::day_1_2, day_2_1::day_2_1, day_2_2::day_2_2,
     day_3_1::day_3_1, day_3_2::day_3_2, day_4_1::day_4_1, day_4_2::day_4_2,
     day_5_1::day_5_1, day_5_2::day_5_2, day_6_1::day_6_1, day_6_2::day_6_2,
+    day_7_1::day_7_1, day_7_2::day_7_2,
     day_not_implemented::day_not_implemented,
 };
 
@@ -32,7 +35,7 @@ const DAYS: [[fn(); 2]; 25] = [
     [day_4_1, day_4_2],
     [day_5_1, day_5_2],
     [day_6_1, day_6_2],
-    [day_not_implemented, day_not_implemented],
+    [day_7_1, day_7_2],
     [day_not_implemented, day_not_implemented],
     [day_not_implemented, day_not_implemented],
     [day_not_implemented, day_not_implemented],
